041f294f46dec8ee10aa5d3b1e8348d2b8bef790
Ramunas P <ramunas.platukis@gmail.com>
2026-02-19 20:59:30 +0200

    supaprastintas configas, instance folderis susikuria paleidus app arba db_populate

--- config.py
+++ config.py
@@ -1,9 +1,3 @@
-import os
-
-basedir = os.path.abspath(
-    os.path.dirname(__file__)
-)
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'NT_projektai_multitable.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///NT_projektai_multitable.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
